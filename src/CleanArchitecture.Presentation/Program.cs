0e85b55f4b67eb5ba76b2cd74c7ddee3335411bd
yussuf-codes <yussuf.code@gmail.com>
2024-08-09 18:39:51 +0300

    configured cors

--- src/CleanArchitecture.Presentation/Program.cs
+++ src/CleanArchitecture.Presentation/Program.cs
@@ -1,4 +1,6 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using CleanArchitecture.Presentation.Extensions;
 using CleanArchitecture.Application.Extensions;
 using CleanArchitecture.Infrastructure.Extensions;
@@ -7,7 +9,7 @@ namespace CleanArchitecture.Presentation;
 
 public static class Program
 {
-    public static void Main()
+    public static async Task Main()
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder();
 
@@ -15,8 +17,22 @@ public static class Program
                         .AddApplicationServices()
                         .AddInfrastructureServices(builder.Configuration);
 
+        string AllowAllOrigins = "AllowAllOrigins";
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(name: AllowAllOrigins, policyBuilder =>
+            {
+                policyBuilder.AllowAnyOrigin();
+                policyBuilder.AllowAnyHeader();
+                policyBuilder.AllowAnyMethod();
+            });
+        });
+
         WebApplication app = builder.Build();
 
-        app.Run();
+        app.UseCors(AllowAllOrigins);
+
+        await app.RunAsync();
     }
 }
